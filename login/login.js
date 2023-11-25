523e41e583d8a2641df67d0a59fcdcc4c9fd1497
SuzaneMendes12 <SuzaneMendes12>
2023-11-25 15:09:44 -0300

    login tela

--- login/login.js
+++ login/login.js
@@ -15,3 +15,39 @@ document.querySelector('#btncadastro')
     formcadastro.style.left = "25px"
     btnColor.style.left = "110px"
 })
+
+
+function validateLogin() {
+  var email = document.getElementById('login').querySelector('input[type="text"]').value;
+  var senha = document.getElementById('login').querySelector('input[type="password"]').value;
+
+  if (!email || !senha) {
+      alert('Por favor, preencha todos os campos para fazer login.');
+      return false;
+  }
+
+  alert('Login bem-sucedido!');
+  return true;
+}
+
+function validateCadastro() {
+  var nome = document.getElementById('cadastro').querySelector('input[placeholder="Nome"]').value;
+  var sobrenome = document.getElementById('cadastro').querySelector('input[placeholder="Sobrenome"]').value;
+  var cpf = document.getElementById('cadastro').querySelector('input[placeholder="CPF"]').value;
+  var email = document.getElementById('cadastro').querySelector('input[placeholder="Email"]').value;
+  var senha = document.getElementById('cadastro').querySelector('input[placeholder="Senha"]').value;
+  var confirmarSenha = document.getElementById('cadastro').querySelector('input[placeholder="Confirmar senha"]').value;
+
+  if (!nome || !sobrenome || !cpf || !email || !senha || !confirmarSenha) {
+      alert('Por favor, preencha todos os campos para cadastrar.');
+      return false;
+  }
+
+  if (senha !== confirmarSenha) {
+      alert('As senhas não coincidem. Por favor, verifique.');
+      return false;
+  }
+
+  alert('Cadastro bem-sucedido!');
+  return true;
+}
