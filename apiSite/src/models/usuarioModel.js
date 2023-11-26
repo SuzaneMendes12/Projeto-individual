2d31080abb45dfcd88edcf30a74fcb10e2c36b41
SuzaneMendes12 <SuzaneMendes12>
2023-11-26 13:19:18 -0300

    organizando o front

--- apiSite/src/models/usuarioModel.js
+++ apiSite/src/models/usuarioModel.js
@@ -1,20 +1,9 @@
 var database = require("../database/config")
 
-// function autenticar(email, senha) {
-//     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-//     var instrucao = `
-//         SELECT id, nome, email, senha FROM usuario WHERE email = '${email}' AND senha = '${senha}';
-//     `;
-//     console.log("Executando a instrução SQL: \n" + instrucao);
-//     return database.executar(instrucao);
-// }
-
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+// cadastrar usuario
 function cadastrar(nome,sobrenome, email, senha)  {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, sobrenome, email, senha);
     
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
     var instrucao = `
             INSERT INTO usuario (nome, sobrenome, email, senha ) VALUES ('${nome}', '${sobrenome}', '${email}', '${senha}');
     `;
@@ -23,6 +12,5 @@ function cadastrar(nome,sobrenome, email, senha)  {
 }
 
 module.exports = {
-    // autenticar,
     cadastrar
 };
