63b4a69b2eec62c9f641e4ece5cf88d57eff0a81
SuzaneMendes12 <SuzaneMendes12>
2023-11-28 21:23:56 -0300

    slide de apresentação

--- site/public/src/models/usuarioModel.js
+++ site/public/src/models/usuarioModel.js
@@ -21,6 +21,17 @@ function cadastrar(nome,sobrenome, email, senha)  {
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
+
+
+model:
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT idUsuario, nome, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
 // function golpeFav (idUsuario, NomeGolpe) {
 //     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():" , idUsuario , NomeGolpe);
 
@@ -41,6 +52,6 @@ function cadastrar(nome,sobrenome, email, senha)  {
 
 
 module.exports = {
-    // autenticar,
+    autenticar,
     cadastrar
 };
--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -11,6 +11,16 @@ function cadastrar(nome,sobrenome, email, senha)  {
     return database.executar(instrucao);
 }
 
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT idUsuario, nome, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
 module.exports = {
-    cadastrar
+    cadastrar,
+    autenticar
 };
